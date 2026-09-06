d636de1d549a931275177a8dcff33e3655552b46
bharath-02 <bharathj222000@gmail.com>
2026-09-06 21:52:31 +0530

    develop product details page

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -3,6 +3,7 @@ import { prisma } from "@/db/prisma";
 import { convertToPlainObject } from "@/lib/utils";
 import { LATEST_PRODUCTS_LIMIT } from "@/lib/constants";
 
+// Get latest products
 export async function getLatestProducts() {
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
@@ -10,4 +11,11 @@ export async function getLatestProducts() {
   });
 
   return convertToPlainObject(data);
+}
+
+// Get single product by it's slug
+export async function getProductBySlug(slug: string) {
+  return await prisma.product.findFirst({
+    where: { slug },
+  });
 }
